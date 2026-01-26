e33c75d80b1188743c52e3dfbbf5fadc15423437
Zechon <83736697+Zechon@users.noreply.github.com>
2026-01-26 10:05:52 -0800

    Netcode Integration V0.8

--- Assets/Scripts/NETCODE/NetcodePlayerMovementSystem.cs
+++ Assets/Scripts/NETCODE/NetcodePlayerMovementSystem.cs
@@ -1,5 +1,6 @@
 using Unity.Burst;
 using Unity.Entities;
+using Unity.Mathematics;
 using Unity.NetCode;
 
 [UpdateInGroup(typeof(PredictedSimulationSystemGroup))]
@@ -12,3 +13,15 @@ partial struct NetcodePlayerMovementSystem : ISystem
         //Getting Started with Netcode for Entities (DOTS Multiplayer Tutorial Unity 6) - 43:22
     }
 }
+
+
+public struct PlayerMovementData : IComponentData
+{
+    public float3 Velocity;
+    public float WalkSpeed;
+    public float SprintSpeed;
+    public float JumpForce;
+    public bool Grounded;
+}
+
+
